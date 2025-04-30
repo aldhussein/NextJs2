c49f6a4795eeb1b9d9a7df402c43837358ecd41b
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-30 18:08:03 +0300

    Update TodoForm.tsx

--- src/app/_components/todo/TodoForm.tsx
+++ src/app/_components/todo/TodoForm.tsx
@@ -45,7 +45,7 @@ const TodoForm = () => {
 
   return (
 
-          <form className='w-full'
+          <form className='w-full mb-20'
           onSubmit={handleSubmit}>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
