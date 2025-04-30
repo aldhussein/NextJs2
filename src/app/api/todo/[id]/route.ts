41544fac522c369388482f6aecd7cbd18625cb78
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-30 12:44:21 +0300

    Update route.ts
    dd last

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -6,7 +6,7 @@ interface Params {
     id: string
 }
 
-export async function PUT(req: NextRequest, {params}: {params: Params}) {
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
 
     await connectToTheDatabase();
 
