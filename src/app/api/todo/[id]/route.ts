fbca11afe31fab052c4a0f91931568a127be7fb6
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-30 12:49:24 +0300

    Update route.ts
    updating all in

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -2,32 +2,19 @@ import connectToTheDatabase from "@/app/lib/db";
 import TodoModel from "@/app/models/todoModel";
 import { NextRequest, NextResponse } from "next/server";
 
-interface Params {
-    id: string
-}
-
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
     await connectToTheDatabase();
-
     const isDone = await req.json();
 
-    const updatedTodo = await TodoModel.findByIdAndUpdate(params.id, {isDone: isDone})
+    const updatedTodo = await TodoModel.findByIdAndUpdate(context.params.id, { isDone });
 
+    return NextResponse.json(updatedTodo, { status: 201 });
+}
 
-    return NextResponse.json(updatedTodo, {status: 201})
-    
-};
-
-
-export async function DELETE(req: NextRequest, {params}: {params: Params}) {
-
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
     await connectToTheDatabase();
 
+    const deletedTodo = await TodoModel.findByIdAndDelete(context.params.id);
 
-    const deletedTodo = await TodoModel.findByIdAndDelete(params.id)
-
-
-    return NextResponse.json(deletedTodo, {status: 201})
-    
+    return NextResponse.json(deletedTodo, { status: 201 });
 }
