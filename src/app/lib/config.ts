1558d42976b8074ffb5cf4f24f785c3a9f120f15
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-30 16:32:09 +0300

    Update config.ts

--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '' :
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://next-js2-topaz.vercel.app/' :
 'http://localhost:3000/'
